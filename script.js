909ce601b97fd0d56df8a112cf5bdee3b6974032
Hana Mohamed <elnegeryh@gmail.com>
2025-08-23 20:08:34 +0300

    Add modal functionality and responsive styles
    Implemented JavaScript to show and hide a modal with overlay on button clicks. Updated CSS for modal responsiveness across different screen sizes, adjusting width and padding for better user experience.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const button = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+const visible = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
+
+for(let i = 0 ; i < button.length ; i++ )
+button[i].addEventListener('click' , visible );
+
+const hide = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+
+    close.addEventListener("click", hide );
+    overlay.addEventListener('click' , hide );
