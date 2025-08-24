d2a6c659698926dae1a36ccf12763dfaa7bd4951
Hana Mohamed <elnegeryh@gmail.com>
2025-08-24 13:22:46 +0300

    Refactor JavaScript for modal functionality
    Updated the script to improve code consistency by standardizing quotation marks and formatting. Enhanced the keyboard accessibility feature for the modal by ensuring the 'Escape' key hides the modal when it is visible.

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
-'use strict';
+"use strict";
 
-const button = document.querySelectorAll('.show-modal');
-const modal = document.querySelector('.modal');
-const close = document.querySelector('.close-modal');
-const overlay = document.querySelector('.overlay');
+const button = document.querySelectorAll(".show-modal");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
 
 const visible = function () {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
-for(let i = 0 ; i < button.length ; i++ )
-button[i].addEventListener('click' , visible );
+for (let i = 0; i < button.length; i++)
+  button[i].addEventListener("click", visible);
 
 const hide = function () {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
-    close.addEventListener("click", hide );
-    overlay.addEventListener('click' , hide );
+close.addEventListener("click", hide);
+overlay.addEventListener("click", hide);
 
-
-    document.addEventListener('keydown', function(e){
-        if (e.key == 'Escape')
-        {
-            if (!modal.classList.contains('hidden')){
-                hide();
-            }
-        }
-    })
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    hide();
+  }
+});
